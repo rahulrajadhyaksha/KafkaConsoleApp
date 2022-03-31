d6518570bcce33390f7098df55baa5a95d34aecc
rajad <rajad@DESKTOP-VG3LPDU>
2022-03-31 11:13:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers",
+                BootstrapServers = "localhost:9092"
+            };
+
+            using (var consumer=new ConsumerBuilder<Null,string>(config).Build())
+            {
+                consumer.Subscribe("temp-topic");
+                while(true)
+                {
+                    var cr = consumer.Consume();
+                    Console.WriteLine(cr.Message.Value);
+                }
+            }
+        }
+    }
+}
